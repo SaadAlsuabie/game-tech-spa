62d5f20c8dc848343eec27309d2e358b90db38b9
Saad Alsuabie <alsuabies@gmail.com>
2024-01-22 02:17:56 +0500

    Added Backend APIs

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const {join} = require("path");
+const fs = require("fs");
 const app = express();
 const PORT = 8000;
 
+
+app.use(bodyParser.json());
+app.use(express.static(join(__dirname, 'public')));
+
+function saveDataToFile(data) {
+    try {
+        fs.writeFileSync("data.json", JSON.stringify(data), 'utf8');
+    } catch (error) {
+        console.error('Error writing to the data file:', error);
+    }
+}
+
+app.post('/json-data', (req, res) => {
+    const { username, characters, seconds } = req.body;
+    console.log(`Received data: username=${username}, characters=${characters}, seconds=${seconds}`);
+    saveDataToFile({ username, characters, seconds });
+    res.json({ message: 'Data received successfully' });
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
